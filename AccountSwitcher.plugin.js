cabaa749f760c92487a7ab802b592d166545f85d
Kevin Faveri <kevinfaveridev@gmail.com>
2021-07-03 16:32:20 -0300

    Update AccountSwitcher.plugin.js

--- AccountSwitcher.plugin.js
+++ AccountSwitcher.plugin.js
@@ -1,51 +1,9 @@
-/**
-* @name AccountSwitcher
-* @displayName AccountSwitcher
-* @source https://github.com/l0c4lh057/AccountSwitcher/blob/master/AccountSwitcher.plugin.js
-* @patreon https://www.patreon.com/l0c4lh057
-* @authorId 226677096091484160
-* @invite YzzeuJPpyj
-*/
-/*@cc_on
-@if (@_jscript)
-	
-	// Offer to self-install for clueless users that try to run this directly.
-	var shell = WScript.CreateObject("WScript.Shell");
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
-	var pathSelf = WScript.ScriptFullName;
-	// Put the user at ease by addressing them in the first person
-	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-	} else if (!fs.FolderExists(pathPlugins)) {
-		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-		// Show the user where to put plugins in the future
-		shell.Exec("explorer " + pathPlugins);
-		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-	}
-	WScript.Quit();
-
-@else@*/
-
 module.exports = (() => {
 	const config = {
 		info: {
 			name: "AccountSwitcher",
-			authors: [
-				{
-					name: "l0c4lh057",
-					discord_id: "226677096091484160",
-					github_username: "l0c4lh057",
-					twitter_username: "l0c4lh057"
-				}
-			],
 			version: "1.3.3",
-			description: "Simply switch between accounts with the ease of pressing a single key.",
-			github: "https://github.com/l0c4lh057/AccountSwitcher",
-			github_raw: "https://raw.githubusercontent.com/l0c4lh057/AccountSwitcher/master/AccountSwitcher.plugin.js"
+			description: "Simply switch between accounts with the ease of pressing a single key."
 		},
 		changelog: [
 			{
